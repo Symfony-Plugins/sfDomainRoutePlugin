9f8061c6a340bdbee7b9610b8c315167f3b7288a
talater <talater@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-02 20:34:00 -0500

    Import of version 0.1.0 to repository.
    git-svn-id: http://svn.symfony-project.com/plugins/sfDomainRoutePlugin/trunk@17887 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/routing/sfDomainRoute.class.php
@@ -0,0 +1,144 @@
+<?php
+
+/*
+ * This file is part of the sfDomainRoute package.
+ * (c) 2009 Tal Ater <tal@talater.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfDomainRoute.
+ *
+ * Based on original code by Fabien Potencier
+ *
+ * @package    sfDomainRoute
+ * @author     Tal Ater <tal@talater.com>
+ */
+
+class sfDomainRoute extends sfRequestRoute
+{
+  /**
+   * Returns true if the URL matches this route, false otherwise.
+   *
+   * @param   string    $url        The URL
+   * @param   array     $context    The context
+   *
+   * @return array      An array of parameters
+   */
+  public function matchesUrl($url, $context = array())
+  {
+    //If method requirement not set, make sure this also works with POST (sfRequestRoute limits it to GET and HEAD)
+    if (!isset($this->requirements['sf_method']))
+    {
+      $this->requirements['sf_method'] = array('get', 'head', 'post');
+    }
+
+    if (false === $retval = parent::matchesUrl($url, $context))
+    {
+      return false;
+    }
+
+    //check host requirements
+    if (isset($this->requirements['sf_host']))
+    {
+      $hostRequirements = $this->requirements['sf_host'];
+      if (!is_array($hostRequirements))
+      {
+        $hostRequirements = array($hostRequirements);
+      }
+      if (!in_array($context['host'], $hostRequirements))
+      {
+        return false;
+      }
+    }
+
+    //get subdomain
+    $subDomain = $this->getSubdomain($context);
+    $retval['subdomain'] = $subDomain;
+
+    return $retval;
+  }
+
+  /**
+   * Returns true if the parameters matches this route, false otherwise.
+   *
+   * @param   mixed     $params     The parameters
+   * @param   array     $context    The context
+   *
+   * @return  Boolean               true if the parameters matches this route, false otherwise.
+   */
+  public function matchesParameters($params, $context = array())
+  {
+    unset($params['subdomain']);
+    return parent::matchesParameters($params, $context);
+  }
+
+  /**
+   * Generates a URL from the given parameters.
+   *
+   * @param   mixed     $params     The parameter values
+   * @param   array     $context    The context
+   * @param   Boolean   $absolute   Whether to generate an absolute URL
+   *
+   * @return  string    The generated URL
+   */
+  public function generate($params, $context = array(), $absolute = false)
+  {
+    $subdomain = isset($params['subdomain']) ? $params['subdomain'] : 'www';
+    unset($params['subdomain']);
+    if ($subdomain && $subdomain != $this->getSubdomain($context))
+    {
+      $url = parent::generate($params, $context, false);
+      return $this->getHostForSubdomain($context, $subdomain).$url;
+    }
+    return parent::generate($params, $context, $absolute);
+  }
+
+  /**
+   * Generates a protocol and host for the given subdomain.
+   *
+   * @param   array     $context    The context
+   * @param   string    $subdomain  subdomain name
+   *
+   * @return  string    The generated protocol and host
+   *
+   * @todo    Currently only supports sub.domain.tld style domains. Modify to support sub.domain.xx.xx
+   */
+  protected function getHostForSubdomain($context, $subdomain)
+  {
+    $parts = explode('.', $context['host']);
+    $partCount = count($parts);
+    if ($partCount>=3)
+    {
+      $parts[0] = $subdomain;
+    }
+    $host = implode('.', $parts);
+    if ($partCount<3) {
+      $host = $subdomain.'.'.$host;
+    }
+    $protocol = 'http'.(isset($context['is_secure']) && $context['is_secure'] ? 's' : '');
+    return $protocol.'://'.$host;
+  }
+
+  /**
+   * Gets the subdomain from the host.
+   *
+   * @param   array     $context    The context
+   *
+   * @return  string    The subdomain part of the host (or null)
+   */
+  protected function getSubdomain($context)
+  {
+    $parts = explode('.', $context['host']);
+    $partCount = count($parts);
+    if ($partCount<3)
+    {
+      return '';
+    } else {
+      return $parts[0];
+    }
+  }
+}
+?>
